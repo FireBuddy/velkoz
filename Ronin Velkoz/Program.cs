9f09468dd99bf27bcb74a7e8ea7b4ac51b38b023
AkumaCORE <k-sintu@hotmail.com>
2016-07-12 15:53:31 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -159,7 +159,7 @@ namespace RoninVelkoz
         private static void QSplitter(EventArgs args)
         {
             // Check if the missile is active
-            if (Handle != null && Core.GameTickCount - QTime <= 3000)
+            if (Handle != null && Core.GameTickCount - QTime <= 1000)
 
             {
                 Chat.Print("Q detected");
