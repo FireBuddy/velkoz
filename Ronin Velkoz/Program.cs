3d3a6c248c2b46c19e7aaaedd4c2f7cac9c2d3c2
AkumaCORE <k-sintu@hotmail.com>
2016-07-23 19:32:08 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -171,7 +171,7 @@ namespace RoninVelkoz
 
                         if (EntityManager.Heroes.Enemies.Count( h => h.IsValidTarget(685) ) > 0)
                         {
-                            Chat.print("Enemy")
+                            Chat.Print("Enemy");
                         }
                         
                         var colliding = collisionObjects
