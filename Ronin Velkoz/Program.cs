ac6f22684858d76b920319d57d809300d0208424
AkumaCORE <k-sintu@hotmail.com>
2016-07-23 19:56:00 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -65,7 +65,7 @@ namespace RoninVelkoz
         public static void OnDraw3(EventArgs args)
         {
              
-             Circle.Draw(SharpDX.Color.Red, 100, 100,Handle.Position.To2D());
+             Circle.Draw(SharpDX.Color.Red, 100, 100, Handle.Position;
         }
 
         public static void UltFollowMode()
