0653d1fea505bbea868b019229aebb1d42c927e3
AkumaCORE <k-sintu@hotmail.com>
2016-07-23 20:50:55 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -201,7 +201,7 @@ namespace RoninVelkoz
                         
                         foreach (var hero in collisionObjects)
                         {
-	                if ( Prediction.Position.Collision.LinearMissileCollision(hero, startPos, endPos + 300, 2000, 400, 250))
+	                if ( Prediction.Position.Collision.LinearMissileCollision(hero, startPos, endPos + 300, 2000, 200, 250))
                     	{
 
                             Chat.Print("Split");
