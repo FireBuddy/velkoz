ea56122213016677b1306b7f19bc759242004561
AkumaCORE <k-sintu@hotmail.com>
2016-07-23 21:39:11 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -164,11 +164,6 @@ namespace RoninVelkoz
                     Chat.Print("oncreat");
                     
                     Handle = missile;
-                    Direction = (missile.EndPosition.To2D() - missile.StartPosition.To2D()).Normalized();
-                    Chat.Print("oncreat2");
-                    Perpendiculars.Add(Direction.Perpendicular());
-                    Perpendiculars.Add(Direction.Perpendicular2());
-                    Chat.Print("oncreat3");
                     QTime = Core.GameTickCount;
                     }
             }
