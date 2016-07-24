484b299b56684e444f79eb1857b90a7d1c80e50b
AkumaCORE <k-sintu@hotmail.com>
2016-07-24 13:50:40 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -84,6 +84,10 @@ namespace RoninVelkoz
         {
         	if (SpellsManager.Q.IsReady())
                 {
+                const float maxAngle = 180f;
+                const float step = maxAngle / 6f;
+                var currentAngle = 0f;
+		var currentStep = 0f;  	
                 }
 
         }
