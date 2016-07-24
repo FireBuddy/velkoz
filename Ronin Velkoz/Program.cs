aebfcbac1acf272cdc6efc045cbc2330fda765c9
AkumaCORE <k-sintu@hotmail.com>
2016-07-24 18:30:58 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -89,7 +89,7 @@ namespace RoninVelkoz
                 	var currentAngle = 0f;
 			var currentStep = 0f;
 			var costom = Math.Cos(currentAngle);
-			var currentvalue = costom;
+			var currentvalue = costom(int);
 			var CurrentTarget = TargetSelector.GetTarget(1500, DamageType.Magical);
 			var direction = (CurrentTarget.Position.To2D() - Champion.Position.To2D()).Normalized();
                         Vector2 checkPoint;
