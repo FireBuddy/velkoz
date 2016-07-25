e452d5ae16469693766623b429b265b02481840f
AkumaCORE <k-sintu@hotmail.com>
2016-07-25 11:47:56 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -104,7 +104,7 @@ namespace RoninVelkoz
 			var intcos = (int)cos;
 			
 			var enemydirection = (CurrentTarget.ServerPosition.To2D() - Champion.ServerPosition.To2D()).Normalized();
-                        var skillshotline = ( Champion.Position.To2D());
+                        var skillshotline = ( enemydirection.Rotated(currentAngle * 1000));
 
                         while (true)    
                         {
