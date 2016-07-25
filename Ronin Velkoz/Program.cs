700b1a349a28c5c677500454134e734ea9bf508a
AkumaCORE <k-sintu@hotmail.com>
2016-07-25 11:40:53 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -41,7 +41,7 @@ namespace RoninVelkoz
         private static MissileClient QMissile;
         private static MissileClient Handle;
         public const float maxAngle = 96f;
-        public static Vector3 intersection;
+        private static Vector3 intersection;
         public static Vector3 intersection2 = intersection;
         //public static Vector3 LastPosition = new Vector3(Handle.Position).To3D(); 
         public static float QTime = 0;
