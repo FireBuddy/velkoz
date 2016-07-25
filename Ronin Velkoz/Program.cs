42e2dbebc24bed793d823da3fdc1ae19741b86b2
AkumaCORE <k-sintu@hotmail.com>
2016-07-25 13:53:58 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -237,7 +237,7 @@ namespace RoninVelkoz
 
                     Chat.Print("oncreat");
                     
-                    //Handle = missile;
+                    Handle = missile;
                     QTime = Core.GameTickCount;
                     }
             }
