a5b13f7df226e6b4e333e45f119c69a0c14e8cb7
AkumaCORE <k-sintu@hotmail.com>
2016-07-25 13:34:02 -0400

    Update Program.cs

--- Ronin Velkoz/Program.cs
+++ Ronin Velkoz/Program.cs
@@ -228,7 +228,7 @@ namespace RoninVelkoz
 
         public static void Obj_AI_Base_OnCreate(GameObject sender, EventArgs args)
         {
-            if (sender.IsAlly && args.SData.Name == "VelkozQMissile")
+            if (sender.IsAlly)
             {
                 Perpendiculars = new List<Vector2>();
                 var missile = (MissileClient)sender;
